934ac69938762f3898b70d146160ad7ddaf34e94
导演 <tao.qit@alibaba-inc.com>
2016-04-19 11:20:29 +0800

    First Commit

---
+++ src/matrix.js
@@ -0,0 +1,127 @@
+/*
+ * @description: You may Reconstruct this method to a Class
+ *
+ */
+function Matrix(options) {
+	
+	var margin = {top: 100, right: 100, bottom: 100, left: 100},
+	    width = 400,
+	    height = 400,
+	    data = options.data,
+	    container = options.container,
+	    labelsData = options.labels,
+	    numrows,
+	    numcols;
+
+	if(!data){
+		throw new Error('No data passed.');
+	}
+
+	if(!Array.isArray(data) || !data.length || !Array.isArray(data[0])){
+		throw new Error('Data type should be two-dimensional Array.');
+	}
+
+	numrows = data.length;
+	numcols = data[0].length;
+
+	var svg = d3.select(container).append("svg")
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom)
+		.append("g")
+	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	var background = svg.append("rect")
+	    .style("stroke", "black")
+	    .style("stroke-width", "2px")
+	    .attr("width", width)
+	    .attr("height", height);
+
+
+	var x = d3.scale.ordinal()
+	    .domain(d3.range(numcols))
+	    .rangeBands([0, width]);
+
+	var y = d3.scale.ordinal()
+	    .domain(d3.range(numrows))
+	    .rangeBands([0, height]);
+
+	var colorMap = d3.scale.linear()
+	    .domain([0, 1])
+	    .range(["white", "black"]);    
+
+	var row = svg.selectAll(".row")
+	    .data(data)
+	  	.enter().append("g")
+	    .attr("class", "row")
+	    .attr("transform", function(d, i) { return "translate(0," + y(i) + ")"; });
+
+	var cell = row.selectAll(".cell")
+	    .data(function(d) { return d; })
+			.enter().append("g")
+	    .attr("class", "cell")
+	    .attr("transform", function(d, i) { return "translate(" + x(i) + ", 0)"; });
+
+	cell.append('rect')    
+	    .attr("width", x.rangeBand())
+	    .attr("height", y.rangeBand())
+	    .style("stroke-width", 0);
+
+    cell.append("text")
+	    .attr("dy", ".32em")
+	    .attr("x", x.rangeBand() / 2)
+	    .attr("y", y.rangeBand() / 2)
+	    .attr("text-anchor", "middle")
+	    .style("fill", function(d, i) { return d >= 0.5 ? 'white' : 'black'; })
+	    .text(function(d, i) { return d; });
+
+	row.selectAll(".cell")
+	    .data(function(d, i) { return data[i]; })
+	    .style("fill", colorMap);
+
+	var labels = svg.append('g')
+		.attr('class', "labels");
+
+	var columnLabels = labels.selectAll(".column-label")
+	    .data(labelsData)
+	  .enter().append("g")
+	    .attr("class", "column-label")
+	    .attr("transform", function(d, i) { return "translate(" + x(i) + "," + height + ")"; });
+
+	columnLabels.append("line")
+		.style("stroke", "black")
+	    .style("stroke-width", "1px")
+	    .attr("x1", x.rangeBand() / 2)
+	    .attr("x2", x.rangeBand() / 2)
+	    .attr("y1", 0)
+	    .attr("y2", 5);
+
+	columnLabels.append("text")
+	    .attr("x", 6)
+	    .attr("y", y.rangeBand() / 2)
+	    .attr("dy", ".32em")
+	    .attr("text-anchor", "end")
+	    .attr("transform", "rotate(-60)")
+	    .text(function(d, i) { return d; });
+
+	var rowLabels = labels.selectAll(".row-label")
+	    .data(labelsData)
+	  .enter().append("g")
+	    .attr("class", "row-label")
+	    .attr("transform", function(d, i) { return "translate(" + 0 + "," + y(i) + ")"; });
+
+	rowLabels.append("line")
+		.style("stroke", "black")
+	    .style("stroke-width", "1px")
+	    .attr("x1", 0)
+	    .attr("x2", -5)
+	    .attr("y1", y.rangeBand() / 2)
+	    .attr("y2", y.rangeBand() / 2);
+
+	rowLabels.append("text")
+	    .attr("x", -8)
+	    .attr("y", y.rangeBand() / 2)
+	    .attr("dy", ".32em")
+	    .attr("text-anchor", "end")
+	    .text(function(d, i) { return d; });
+
+}
